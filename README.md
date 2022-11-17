d3817f64a74bf8af122235d15c02a24ab0dd82c4
Luca De Menego <lucademenego99@gmail.com>
2022-11-17 13:14:42 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# on-demand-sdn-slices
+
+Topology: mesh with 5 hosts and 5 switches.
+
+Spanning_tree_switch taken from: /usr/lib/python3/dist-packages/ryu/app/simple_switch_stp_13.py
+
+
+### Slice test 1 - hosts 1 and 2
+
+sudo ovs-vsctl set port s1-eth5 qos=@newqos -- \
+--id=@newqos create QoS type=linux-htb \
+other-config:max-rate=10000000000 \
+queues:1=@1q -- \
+--id=@1q create queue other-config:min-rate=10000 other-config:max-rate=500000
+
+sudo ovs-vsctl set port s2-eth5 qos=@newqos -- \
+--id=@newqos create QoS type=linux-htb \
+other-config:max-rate=10000000000 \
+queues:1=@1q -- \
+--id=@1q create queue other-config:min-rate=10000 other-config:max-rate=500000
+
+sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.1,nw_dst=10.0.0.2,idle_timeout=0,actions=set_queue:1,normal
+
+sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.2,nw_dst=10.0.0.1,idle_timeout=0,actions=set_queue:1,normal
---
+++ mesh_network.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import OVSKernelSwitch, RemoteController
+from mininet.cli import CLI
+from mininet.link import TCLink
+
+
+class MeshTopo(Topo):
+    def __init__(self):
+        # Initialize topology
+        Topo.__init__(self)
+
+        # Create template host, switch, and link
+        host_config = dict(inNamespace=True)
+        link_config = dict(bw=10)
+        host_link_config = dict()
+
+        # Keep list of switches and hosts
+        switches = []
+        hosts = []
+
+        # Create switch nodes
+        for i in range(5):
+            sconfig = {"dpid": "%016x" % (i + 1)}
+            switches.append(self.addSwitch("s%d" % (i + 1), **sconfig))
+
+        # Create host nodes
+        for i in range(5):
+            hosts.append(self.addHost("h%d" % (i + 1), **host_config))
+
+        # Add switch links
+        for i in range(0, len(switches)):
+               	for j in range(i+1, len(switches)):
+                       	self.addLink(switches[i], switches[j], **link_config)
+
+        # Add host links
+       	for i in range(0, len(hosts)):
+                self.addLink(hosts[i], switches[i], **host_link_config)
+
+
+topos = {"meshtopo": (lambda: MeshTopo())}
+
+if __name__ == "__main__":
+    topo = MeshTopo()
+    net = Mininet(
+        topo=topo,
+        switch=OVSKernelSwitch,
+        build=False,
+        autoSetMacs=True,
+        autoStaticArp=True,
+        link=TCLink,
+    )
+    controller = RemoteController("c1", ip="127.0.0.1", port=6633)
+    net.addController(controller)
+    net.build()
+    net.start()
+    CLI(net)
+    net.stop()
